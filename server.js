8b20b8316e89007f2520016828fe164602e0023a
reihaniz <reihaniz.pr@gmail.com>
2024-06-16 15:34:32 +0700

    Small Fixes

--- server.js
+++ server.js
@@ -5,143 +5,205 @@ const mysql = require('mysql2');
 const jwt = require('jsonwebtoken');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'destinatik'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'destinatik'
 });
 connection.connect((err) => {
-  if (err) {
-    console.error('Gagal terhubung ke database:', err);
-    return;
-  }
-  console.log('Terhubung ke database!');
+    if (err) {
+        console.error('Gagal terhubung ke database:', err);
+        return;
+    }
+    console.log('Terhubung ke database!');
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Pake POST yah!');
+    res.send('Pake POST yah!');
 });
 app.post('/register', (req, res) => {
-  const { username, email, password } = req.body;
+    const {
+        username,
+        email,
+        password
+    } = req.body;
 
-  if (!username || !email || !password) {
-    return res.status(400).json({ error: 'Semua field harus diisi', 'yang di isi': req.body });
-  }
-
-  connection.query(
-    'INSERT INTO user (id, username, email, password, token) VALUES (NULL, ?, ?, ?, ?)',
-    [username, email, password, '?'],
-    (error, result) => {
-      if (error) {
-        console.error(error);
-        return res.status(500).json({ error });
-      }
-
-      res.status(201).json({ status: '1', id: result.insertId, username, email });
+    if (!username || !email || !password) {
+        return res.status(400).json({
+            error: 'Semua field harus diisi',
+            'yang di isi': req.body
+        });
     }
-  );
+
+    connection.query(
+        'INSERT INTO user (id, username, email, password, token) VALUES (NULL, ?, ?, ?, ?)',
+        [username, email, password, '?'],
+        (error, result) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({
+                    error
+                });
+            }
+
+            res.status(201).json({
+                status: '1',
+                id: result.insertId,
+                username,
+                email
+            });
+        }
+    );
 });
 app.post('/login', (req, res) => {
-  const { usernameOrEmail, password } = req.body;
+    const {
+        usernameOrEmail,
+        password
+    } = req.body;
 
-  if (!usernameOrEmail || !password) {
-    return res.status(400).json({ error: 'Username/email dan password harus diisi' });
-  }
-
-  connection.query(
-    'SELECT * FROM user WHERE username = ? OR email = ?',
-    [usernameOrEmail, usernameOrEmail],
-    (err, rows, fields) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Terjadi kesalahan saat mengakses database' });
-      }
-
-      if (rows.length === 0) {
-        return res.status(401).json({ error: 'Username/email atau password salah' });
-      }
-
-      const user = rows[0];
-      if (user.password === password) {
-        const token = jwt.sign({ data: usernameOrEmail }, 'shhhhh');
-		// Update token di database
-        const updateQuery = "UPDATE user SET token = ? WHERE username = ? OR email = ?";
-        connection.query(updateQuery, [token, usernameOrEmail, usernameOrEmail], (err, result) => {
-			if (err) {
-				console.error(err);
-				return res.status(500).json({ error: 'Terjadi kesalahan saat memperbarui token' });
-			}
-			res.set('Authorization', `Bearer ${token}`);
-			return res.json({ 'usernameOrEmail': usernameOrEmail, status: '1' });
-		});
-      } else {
-        return res.status(401).json({ error: 'Username/email atau password salah' });
-      }
+    if (!usernameOrEmail || !password) {
+        return res.status(400).json({
+            error: 'Username/email dan password harus diisi'
+        });
     }
-  );
+
+    connection.query(
+        'SELECT * FROM user WHERE username = ? OR email = ?',
+        [usernameOrEmail, usernameOrEmail],
+        (err, rows, fields) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({
+                    error: 'Terjadi kesalahan saat mengakses database'
+                });
+            }
+
+            if (rows.length === 0) {
+                return res.status(401).json({
+                    error: 'Username/email atau password salah'
+                });
+            }
+
+            const user = rows[0];
+            if (user.password === password) {
+                const token = jwt.sign({
+                    data: usernameOrEmail
+                }, 'shhhhh');
+                // Update token di database
+                const updateQuery = "UPDATE user SET token = ? WHERE username = ? OR email = ?";
+                connection.query(updateQuery, [token, usernameOrEmail, usernameOrEmail], (err, result) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).json({
+                            error: 'Terjadi kesalahan saat memperbarui token'
+                        });
+                    }
+                    res.set('Authorization', `Bearer ${token}`);
+                    return res.json({
+                        'usernameOrEmail': usernameOrEmail,
+                        status: '1'
+                    });
+                });
+            } else {
+                return res.status(401).json({
+                    error: 'Username/email atau password salah'
+                });
+            }
+        }
+    );
 });
 app.get('/list', (req, res) => {
-  const sql = 'SELECT * FROM `user`';
-  connection.query(sql, (err, rows, fields) => {
-    if (err instanceof Error) {
-      return res.status(500).json({ err });
-    }
-    res.json({rows});
-  });
+    const sql = 'SELECT * FROM `user`';
+    connection.query(sql, (err, rows, fields) => {
+        if (err instanceof Error) {
+            return res.status(500).json({
+                err
+            });
+        }
+        res.json({
+            rows
+        });
+    });
 });
 app.post('/rating/rate', (req, res) => {
-  const { userId, movieId, rating } = req.body;
+    const {
+        userId,
+        placeId,
+        rating
+    } = req.body;
 
-  if (!userId || !movieId || !rating) {
-    return res.status(400).json({ error: 'Semua field harus diisi' });
-  }
-
-  connection.query(
-    'INSERT INTO ratings (id, userId, movieId, rating) VALUES (NULL, ?, ?, ?)',
-    [userId, movieId, rating],
-    (error, result) => {
-      if (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Terjadi kesalahan saat menyimpan rating' });
-      }
-
-      res.status(201).json({ status: '1', id: result.insertId, userId, movieId, rating });
+    if (!userId || !placeId || !rating) {
+        return res.status(400).json({
+            error: 'Semua field harus diisi'
+        });
     }
-  );
+
+    connection.query(
+        'INSERT INTO rating (id, userId, placeId, rating) VALUES (NULL, ?, ?, ?)',
+        [userId, placeId, rating],
+        (error, result) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({
+                    error: 'Terjadi kesalahan saat menyimpan rating'
+                });
+            }
+
+            res.status(201).json({
+                status: '1',
+                id: result.insertId,
+                userId,
+                placeId,
+                rating
+            });
+        }
+    );
 });
 app.get('/rating/list', (req, res) => {
-  const placeId = req.query.placeId;
+    const placeId = req.query.placeId;
 
-  let sql;
-  if (placeId) {
-    sql = 'SELECT * FROM `rating` WHERE `placeId` = ?';
-    connection.query(sql, [placeId], (err, rows, fields) => {
-      if (err) {
-        return res.status(500).json({ error: err.message });
-      }
-      res.json({ rows });
-    });
-  } else {
-    sql = 'SELECT * FROM `rating`';
-    connection.query(sql, (err, rows, fields) => {
-      if (err) {
-        return res.status(500).json({ error: err.message });
-      }
-      res.json({ rows });
-    });
-  }
+    let sql;
+    if (placeId) {
+        sql = 'SELECT * FROM `rating` WHERE `placeId` = ?';
+        connection.query(sql, [placeId], (err, rows, fields) => {
+            if (err) {
+                return res.status(500).json({
+                    error: err.message
+                });
+            }
+            res.json({
+                rows
+            });
+        });
+    } else {
+        sql = 'SELECT * FROM `rating`';
+        connection.query(sql, (err, rows, fields) => {
+            if (err) {
+                return res.status(500).json({
+                    error: err.message
+                });
+            }
+            res.json({
+                rows
+            });
+        });
+    }
 });
 app.get('/rating/average', (req, res) => {
-  const placeId = req.query.placeId;
+    const placeId = req.query.placeId;
 
-  if (!placeId) {
-    return res.status(400).json({ error: 'placeId parameter is required' });
-  }
+    if (!placeId) {
+        return res.status(400).json({
+            error: 'placeId parameter is required'
+        });
+    }
 
-  const sql = `
+    const sql = `
     SELECT 
       COUNT(*) AS total_ratings,
       AVG(rating) AS average_rating
@@ -151,15 +213,17 @@ app.get('/rating/average', (req, res) => {
       placeId = ?
   `;
 
-  connection.query(sql, [placeId], (err, rows, fields) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json(rows[0]);
-  });
+    connection.query(sql, [placeId], (err, rows, fields) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+        res.json(rows[0]);
+    });
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+    console.log(`Server listening on port ${PORT}...`);
 });
